5b3ed65f4227e28963bcb1611b3b015e7e8715fc
Lolan <102511853+Jeolpaul@users.noreply.github.com>
2022-06-18 14:05:57 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/Jeolpaul/Doctor-Strange-Autofilter /Doctor-Strange-Autofilter
+  git clone https://github.com/Jeolpaul/George-MalarAutofilter.git /George-Malar-Autofilter
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /Doctor-Strange-Autofilter
+  git clone $UPSTREAM_REPO /George-Malar-Autofilter
 fi
-cd /Doctor-Strange-Autofilter
+cd /George-Malar-Autofilter
 pip3 install -U -r requirements.txt
 echo "Starting BETA Botz💫...."
 python3 bot.py
