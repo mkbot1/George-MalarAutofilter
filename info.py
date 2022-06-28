728f0c50891a75e9bdb76144914d39227b3b360d
AS ALWAYS NOOB! <102511853+Jeolpaul@users.noreply.github.com>
2022-06-28 19:22:34 +0530

    Update info.py

--- info.py
+++ info.py
@@ -15,7 +15,10 @@ SESSION = environ.get('SESSION', 'Media_search')
 API_ID = int(environ['API_ID'])
 API_HASH = environ['API_HASH']
 BOT_TOKEN = environ['BOT_TOKEN']
+
+# Bot Decoraters
 GROUP_SUPPORT = environ['GROUP_SUPPORT']
+UPDATES_CHANNEL = environ['UPDATES_CHANNEL']
 
 # Bot settings
 CACHE_TIME = int(environ.get('CACHE_TIME', 300))
